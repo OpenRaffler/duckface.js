7f43811312fee5726e64f4cc29a4a946269deecc
Tom Van Herreweghe <tom@king-foo.be>
2015-03-25 15:27:34 +0100

    Correct checking if called as constructor
    Signed-off-by: Tom Van Herreweghe <tom@king-foo.be>

--- src/duckface.js
+++ src/duckface.js
@@ -52,7 +52,7 @@ define(function (require) {
             throw new Error ("Duckface constructor called with " + arguments.length + "arguments, but expected exactly " + Duckface.length);
         }
 
-        if (this.constructor != Duckface) {
+        if (typeof this === undefined || this.constructor != Duckface) {
             throw new Error ("Duckface should be called as a constructor");
         }
 
